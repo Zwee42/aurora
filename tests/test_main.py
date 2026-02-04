506c164e76a082667ae95456b4b3f5464b485da0
Yannick Winkler <yannickwinkler3@gmail.com>
2026-02-04 14:22:58 +0100

    Modified test file to mock input requirements

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,7 +5,8 @@ from unittest.mock import patch
 from aurora.main import main
 
 class Test_main(unittest.TestCase):
-    def test_main_contains_packages_require_attention(self):
+    @patch("builtins.input", return_value="n")
+    def test_main_contains_packages_require_attention(self, mock_input):
         with patch("sys.stdout", new_callable=StringIO) as fake_out:
             main()
             output = fake_out.getvalue()
